97e401f639cd5864e7bed3634543970c81c2a7d1
(bot) Dipsy Wong <ycwongal@connect.ust.hk>
2023-12-11 17:27:35 +0800

    auto update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate'
+  })],
   base: '/old-calendar/'
 })
