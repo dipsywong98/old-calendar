e1713c5041b422649c7feccda21c5fb324650292
(bot) Dipsy Wong <ycwongal@connect.ust.hk>
2023-12-11 11:40:16 +0800

    fix build

--- src/Highlighter.tsx
+++ src/Highlighter.tsx
@@ -16,24 +16,22 @@ const basicColorMap: Record<string, string> = {
 
 const colorMap = {
   ...basicColorMap,
-  ...Object.fromEntries(Object.entries(干轉五運).flatMap(([干, 五運]) => [
+  ...Object.fromEntries(Object.values(干轉五運).map((五運) => [
     // [干, basicColorMap[五運[0]]],
     [五運, basicColorMap[五運[0]]]
   ])),
-  ...Object.fromEntries(Object.entries(支轉六氣).flatMap(([支, 六氣]) => [
+  ...Object.fromEntries(Object.values(支轉六氣).map((六氣) => [
     // [支, basicColorMap[六氣[3]]],
     [六氣, basicColorMap[六氣[3]]]
   ]))
 }
-
-console.log(colorMap)
 
 export const Highlighter = ({children}: Props) => {
   const keywordRegex = new RegExp(Object.keys(colorMap).sort((a, b) => b.length - a.length).map(key => `(${key})`).join('|'))
   const words = children.split(keywordRegex).filter((w) => w?.length > 0)
   return (
     <Typography>
-      {words.map((word, index) => <Box component='span' sx={{ color: colorMap[word] }}>{word}</Box>)}
+      {words.map((word) => <Box component='span' sx={{ color: colorMap[word] }}>{word}</Box>)}
     </Typography>
   )
 }
